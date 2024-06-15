d707414118a2b5d3a12bfd6879cb22d6054b7a6c
dilshan488 <dilshanpubudu461@gmail.com>
2024-06-15 15:45:08 +0530

    create a middleware

--- api/index.js
+++ api/index.js
@@ -1,26 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/authe.route.js';
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/authe.route.js"
 
-dotenv.config();
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to DB');
-    }).catch((err)=> {
-console.log(err);
-    });
+dotenv.config()
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB")
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
+const app = express()
+app.use(express.json())
+app.listen(3000, () => {
+  console.log("Server is running on 3000!")
+})
 
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
-const app = express();
-app.use(express.json());
-app.listen(3000,()=>{
-
-    console.log('Server is running on 3000!');
-});
-
-
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Server Error"
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+})
